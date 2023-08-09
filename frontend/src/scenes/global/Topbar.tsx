f991df65531c5f43da07cb2d49d95a86cf394931
ricardomart26 <ricardomartins2626t@gmail.com>
2023-08-09 19:00:34 +0100

    Fixed submodule

---
+++ frontend/src/scenes/global/Topbar.tsx
@@ -0,0 +1,6 @@
+
+const Topbar = () => {
+    return (<div>Topbar</div>);
+}
+
+export default Topbar;
---
+++ frontend/src/theme.tsx
@@ -0,0 +1,9 @@
+import { createContext, useState, useMemo } from "react";
+import { createTheme } from "@mui/material/styles";
+
+// Color design tokens
+#666666
+#141b2d
+#4cceac
+#db4f4a
+#6870fa
